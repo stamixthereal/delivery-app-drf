77bf7145dc39010e07b3b335c977498a59c81e1f
stamixthereal <staskazanov@gmail.com>
2021-12-12 13:28:44 +0300

    customer's model, setting up env

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Customer(models.Model):
+    """Customer's model"""
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(unique=True)
+    phone = PhoneNumberField(unique=True)
