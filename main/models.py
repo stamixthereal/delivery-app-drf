a8bd0b5f897499fea156ade29cad8d34771cec8e
stamixthereal <staskazanov@gmail.com>
2021-12-12 14:19:05 +0300

    adress, order models

--- main/models.py
+++ main/models.py
@@ -1,11 +1,38 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from datetime import datetime
+from django_countries.fields import CountryField
 
 
 class Customer(models.Model):
-    """Customer's model"""
+    """Customer model"""
 
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(unique=True)
     phone = PhoneNumberField(unique=True)
+
+
+class Adress(models.Model):
+    """Adress model"""
+
+    country = CountryField(blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    street = models.CharField(max_length=150, blank=True)
+    house = models.IntegerField(blank=True)
+
+
+class OrderDish(models.Model):
+    """Dish per order"""
+
+    dish_name = ''
+    quantity = ''
+    price = ''
+
+class Order(models.Model):
+    """Order model"""
+
+    date = models.DateField(default=datetime.now, blank=True)
+    customer = models.ForeignKey(Customer)
+    adress = models.ForeignKey(Adress, on_delete=models.CASCADE)
+    dishes = models.ForeignKey(OrderDish, on_delete=models.CASCADE)
